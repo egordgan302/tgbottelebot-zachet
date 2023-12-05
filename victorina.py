f3c58864e0d3f0bc59451f5547e29fd6c1e57442
egor <troyanviskov@gmail.com>
2023-12-05 21:42:48 +0300

    new file:   questions.txt new file:   victorina.py

---
+++ victorina.py
@@ -0,0 +1,26 @@
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("6575506276:AAERV4hw8lignyyBbFshkkdUK3vAEddtIHo")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(types.KeyboardButton("ВИКТОРИНА"))
+    bot.send_message(message.chat.id, "Привет, давай поиграем в викторину?", reply_markup=markup)
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+    if message.text.lower() == "да":
+        ask_questions(message.chat.id)
+
+def ask_questions(chat_id):
+    with open("questions.txt", "r") as file:
+        questions = file.readlines()
+    for question in questions:
+        bot.send_message(chat_id, question.strip())
+
+bot.polling()
+
+
+    
