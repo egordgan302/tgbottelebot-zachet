ed76ed9e144685f7e488f0c217a1fc9cbe8a8ab7
egor <troyanviskov@gmail.com>
2023-12-10 15:35:36 +0300

    completed project on Telebot

--- victorina.py
+++ victorina.py
@@ -1,46 +1,66 @@
 import telebot
-from telebot import types
+import json
+
+bot = telebot.TeleBot("")
 
 
-bot = telebot.TeleBot("6575506276:AAERV4hw8lignyyBbFshkkdUK3vAIHo")
 try:
-    with open('questions.txt', 'r') as file:
+    with open('questions.json', 'r', encoding='utf-8') as file:
         questions = file.readlines()
 except FileNotFoundError:
-    print("File questions.txt not found")
+    print("File questions.json not found")
     exit()
+def read_questions(file):
+    with open(file, 'r', encoding='utf-8') as f:
+        questions = json.load(f)
+        return questions
 
-score = 0
+questions_file = 'questions.json'
+quiz_questions = read_questions(questions_file)
+user_answers = {}  
 
-# Функция для отправки вопроса и кнопок
-def send_question(message):
-    global score
-    if questions:
-        question = questions.pop(0).strip()
-        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        options = ["Option 1", "Option 2", "Option 3", "Option 4"]
-        for option in options:
-            markup.add(types.KeyboardButton(option))
-        bot.send_message(message.chat.id, question, reply_markup=markup)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "dota 2: Welcome to the world of absurdity {0.first_name}! This quiz is a survival game, during the quiz your soul belongs to me, good luck? /start_quiz".format(message.from_user))
+
+@bot.message_handler(commands=['start_quiz'])
+def start_quiz(message):
+    chat_id = message.chat.id
+    user_answers[chat_id] = {'score': 0, 'current_question': 0}
+    send_question(chat_id)
+
+def send_question(chat_id):
+    current_question = user_answers[chat_id]['current_question']
+    if current_question < len(quiz_questions):
+        question_data = quiz_questions[current_question]
+        question = question_data['question']
+        options = question_data['options']
+        markup = create_markup(options)
+        bot.send_message(chat_id, f"{question}\n\nchoose your path:", reply_markup=markup)
     else:
-        score = 0
-        bot.send_message(message.chat.id, f"The quiz is over. Your account: {score}")
-        questions.clear()       
+        finish_quiz(chat_id)
 
+def create_markup(options):
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    for option in options:
+        markup.add(telebot.types.KeyboardButton(option))
+    return markup
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Welcome to the world of absurdity {0.first_name}! This quiz is a survival game, during the quiz your soul belongs to me, good luck".format(message.from_user))
-# Обработчик ответов пользователя
+def finish_quiz(chat_id):
+    score = user_answers[chat_id]['score']
+    bot.send_message(chat_id, f"The quiz is over. souls remained:: {score}/{len(quiz_questions)}")
+
 @bot.message_handler(func=lambda message: True)
-def handle_answer(message):
-    global score
-    if message.text == "Correct option":
-        score += 1
-    send_question(message)
+def check_answer(message):
+    chat_id = message.chat.id
+    current_question = user_answers[chat_id]['current_question']
+    correct_answer = quiz_questions[current_question]['answer']
+    user_answer = message.text
 
-# Запускаем бота
-bot.polling()
+    if user_answer == correct_answer:
+        user_answers[chat_id]['score'] += 1
 
+    user_answers[chat_id]['current_question'] += 1
+    send_question(chat_id)
 
-    
+bot.polling(none_stop=True)
